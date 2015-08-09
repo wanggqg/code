3f14810a26c0cc4a14b8691919e2b856aa39cccc
wangguoqiang <1459939283@qq.com>
2015-08-10 02:37:00 +0800

    这个How被管理起来了

---
+++ HowOld/app/src/main/java/com/app/wangguoqiang/howold/MainActivity.java
@@ -0,0 +1,48 @@
+package com.app.wangguoqiang.howold;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private ImageView iv;
+    private Button btIamge;
+    private Button btHow;
+    private String mCurrentPhoto = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        assignViews();
+        initEvents();
+    }
+
+    private void initEvents() {
+        btIamge.setOnClickListener(this);
+        btHow.setOnClickListener(this);
+    }
+
+    private void assignViews() {
+        iv = (ImageView) findViewById(R.id.iv);
+        btIamge = (Button) findViewById(R.id.bt_iamge);
+        btHow = (Button) findViewById(R.id.bt_how);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_iamge:
+                Intent intent = new Intent(Intent.ACTION_PICK);
+
+                break;
+            case R.id.bt_how:
+
+                break;
+        }
+    }
+}
